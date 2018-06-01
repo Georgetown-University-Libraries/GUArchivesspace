97580ff48a879aacdde304a24e9fb0df8ed54179
Michael Marttila <michael.marttila@georgetown.edu>
2018-06-01 14:10:13 -0400

    Modify aeon_web_url

--- overlays/config/config.rb
+++ overlays/config/config.rb
@@ -630,27 +630,27 @@ AppConfig[:pui_page_actions_request] = false
 ##Aeon Fulfillment Plugin
 AppConfig[:aeon_fulfillment] = {
 	"brl" => {
-		:aeon_web_url => "https://aeon.library.georgetown.edu/aeon/aeon.dll",
+		:aeon_web_url => "https://aeon.library.georgetown.edu/logon/",
 		:aeon_return_link_label => "Return to ArchivesSpace",
 		:aeon_external_system_id => "ArchivesSpace"
 	},
 	"gta" => {
-		:aeon_web_url => "https://aeon.library.georgetown.edu/aeon/aeon.dll",
+		:aeon_web_url => "https://aeon.library.georgetown.edu/logon/",
 		:aeon_return_link_label => "Return to ArchivesSpace",
 		:aeon_external_system_id => "ArchivesSpace"
 	},
 	"woodstock" => {
-		:aeon_web_url => "https://aeon.library.georgetown.edu/aeon/aeon.dll",
+		:aeon_web_url => "https://aeon.library.georgetown.edu/logon/",
 		:aeon_return_link_label => "Return to ArchivesSpace",
 		:aeon_external_system_id => "ArchivesSpace"
 	},
 	"gtrb" => {
-		:aeon_web_url => "https://aeon.library.georgetown.edu/aeon/aeon.dll",
+		:aeon_web_url => "https://aeon.library.georgetown.edu/logon/",
 		:aeon_return_link_label => "Return to ArchivesSpace",
 		:aeon_external_system_id => "ArchivesSpace"
 	},
 	"gtm" => {
-		:aeon_web_url => "https://aeon.library.georgetown.edu/aeon/aeon.dll",
+		:aeon_web_url => "https://aeon.library.georgetown.edu/logon/",
 		:aeon_return_link_label => "Return to ArchivesSpace",
 		:aeon_external_system_id => "ArchivesSpace"
 	}
