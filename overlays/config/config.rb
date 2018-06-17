f68ba8c6e63443f2818256065d33ec5cf8c7e449
Michael Marttila <michael.marttila@georgetown.edu>
2018-06-16 21:24:26 -0400

    Add Excel plugin load to config

--- overlays/config/config.rb
+++ overlays/config/config.rb
@@ -67,7 +67,7 @@ AppConfig[:backend_log_level] = "info"
 #AppConfig[:locale] = :en
 #
 ## Plug-ins to load. They will load in the order specified
-AppConfig[:plugins] = ['local',  'aeon_fulfillment']
+AppConfig[:plugins] = ['local',  'aeon_fulfillment', 'aspace_import_excel']
 #
 ## The number of concurrent threads available to run background jobs
 ## Introduced for AR-1619 - long running jobs were blocking the queue
